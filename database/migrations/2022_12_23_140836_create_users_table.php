ae80bf6b855e45bb616a7e2acd92da3a713075dc
bangsiren <bangsir61@gmail.com>
2023-01-01 19:06:50 -1100

    updates

--- database/migrations/2022_12_23_140836_create_users_table.php
+++ database/migrations/2022_12_23_140836_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('age');
             $table->string('email')->unique();
             $table->string('password');
+            <$table->string('password_confirmation');
             $table->timestamps();
         });
     }
