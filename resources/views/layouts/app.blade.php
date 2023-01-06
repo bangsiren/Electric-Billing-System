082929f99a9f0e61696a806d2c46d19cff87f89c
bangsiren <bangsir61@gmail.com>
2023-01-06 03:14:20 -1100

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,61 +22,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-lg  navbar-expand-md navbar-dark bg-primary bg-gradient">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                
-                            <li class='nav-item'><a class="nav-link" href="">Login</a></li>
-                            <li class='nav-item'><a class="nav-link" href="">Register</a></li>
-                
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle text-light fw-bold" data-bs-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    Rene </span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href=""
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                   
-                    </ul>
-                </div>
-            </div>
-        </nav>
+     
 
         @yield('content')
     </div>
