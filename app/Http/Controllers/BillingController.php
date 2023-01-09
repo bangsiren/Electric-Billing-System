eb30b1c4777e81f4b6f83c52951c7da6efdc7c96
bangsiren <bangsir61@gmail.com>
2023-01-09 02:01:28 -1100

    updates

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bill;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BillingController extends Controller
 {
@@ -21,5 +23,25 @@ class BillingController extends Controller
     public function index()
     {
         return view('home');
+    }
+    public function addBill(Request  $request){
+         $request->validate([
+            'connectionId' => 'required',
+            'initial' => 'required',
+            'final' => 'required',
+            'customerId'=> 'required|min:5|max:10'
+         ]);
+         $bill = new Bill;
+         $bill->customerId=$request->connectionId;
+         $bill->initial=$request->initial;
+         $bill->final=$request->final;
+         $bill->month=$request->month;
+         $bill->year=$request->year;
+         $bill->units=(integer)$bill->final-(integer)$bill->initial;
+         $admin=DB::table('admins')->first();
+         $rate=$admin->rate;
+         $bill->amount=$bill->units * $rate;
+         $bill->status="Unpaid";
+         $bill->save();
     }
 }
